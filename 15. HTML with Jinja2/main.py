87059bf0125d7a202e61a9166062211672897552
donghyun kang <48580174+abs-git@users.noreply.github.com>
2023-01-15 15:38:28 +0900

    fix

--- 15. HTML with Jinja2/main.py
+++ 15. HTML with Jinja2/main.py
@@ -55,31 +55,3 @@ def post_form(request: Request, form_data: AwesomForm = Depends(AwesomForm.as_fo
     context = {'request': request}
     return templates.TemplateResponse('awesome.html', context) 
 
-
-################################################################################################
-# file upload
-
-UPLOAD_FILES_DIR = './uploaded_files/'
-
-def generate_html_response():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
-    return HTMLResponse(content=html_content, status_code=200)
-
-
-@app.post('/uploadfile/', response_class=HTMLResponse)
-async def create_upload_file(files: List[UploadFile] = File(...)):
-    for data in files:
-        with open(UPLOAD_FILES_DIR + f'{data.filename}', 'wb') as buffer:
-            shutil.copyfileobj(data.file, buffer)
-
-    return generate_html_response()
-
